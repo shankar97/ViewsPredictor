13b4074c48839a49bc8735eab22401840973e765
Shankar Dutt Salwan <Shankar@dhcp-10-105-160-237.wireless.northwestern.private>
2018-06-10 20:50:13 -0500

    Testing 100 + merge dataset

--- script.py
+++ script.py
@@ -11,16 +11,28 @@ from sklearn.metrics import classification_report,confusion_matrix,accuracy_scor
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn import tree
 import graphviz
+from sklearn.model_selection import cross_val_score
+from sklearn.tree._tree import TREE_LEAF
 
-
+def prune_index(inner_tree, index, threshold):
+    if inner_tree.value[index].min() < threshold:
+        # turn node into a leaf by "unlinking" its children
+        inner_tree.children_left[index] = TREE_LEAF
+        inner_tree.children_right[index] = TREE_LEAF
+    # if there are shildren, visit them as well
+    if inner_tree.children_left[index] != TREE_LEAF:
+        prune_index(inner_tree, inner_tree.children_left[index], threshold)
+        prune_index(inner_tree, inner_tree.children_right[index], threshold)
 
 
 # pdb.set_trace()
 # dataset = numpy.loadtxt("download (14).csv", delimiter=",",skiprows=1)
 dataset = pd.read_csv("dataset/download (18).csv")
-dataset = dataset.drop("DislikeCount",axis=1)
-dataset = dataset.drop("DislikeCountRounded",axis=1)
-dataset = dataset.drop("LikeDislikeRatio",axis=1)
+# dataset = dataset.drop("DislikeCount",axis=1)
+# dataset = dataset.drop("DislikeCountRounded",axis=1)
+# dataset = dataset.drop("LikeDislikeRatio",axis=1)
+dataset = dataset.drop("LikeCount",axis=1)
+dataset = dataset.drop("ViewCount",axis=1)
 # print dataset.describe()
 
 df2 = dataset.columns.get_values()
@@ -29,6 +41,10 @@ df2 = df2.tolist()
 
 X = dataset.drop("LikedOrDisliked",axis = 1)
 Y = dataset["LikedOrDisliked"]
+
+acc_MLP = []
+acc_DTG = []
+acc_DTE = []
 
 ## MLP implmented using 100 layers
 X_train, X_test, y_train, y_test = train_test_split(X, Y)
@@ -40,31 +56,40 @@ X_test = scaler.transform(X_test)
 
 mlp = MLPClassifier(hidden_layer_sizes=(100,200,100),max_iter=600)
 mlp.fit(X_train,y_train)
+for i in range(0,100):
+	mlp.fit(X_train,y_train)
+	predictions = mlp.predict(X_test)
+	acc_MLP.append(accuracy_score(y_test,predictions)*100)
 
-predictions = mlp.predict(X_test)
+scores_MLP = cross_val_score(mlp, X, Y, cv=10)
 
-print(classification_report(y_test,predictions))
-print "MLP accuracy using 100 hidden layers: " + str(accuracy_score(y_test,predictions)*100)
+print "MLP accuracy using 100 hidden layers: " + str(sum(acc_MLP)/len(acc_MLP))
 
 ## Decision Tree implementation
 # pdb.set_trace()
 X_train, X_test, y_train, y_test = train_test_split( X, Y, test_size = 0.3)
 
-clf_gini = DecisionTreeClassifier(criterion = "gini", max_depth=10, min_samples_leaf=2)
-clf_gini.fit(X_train, y_train)
+clf_gini = DecisionTreeClassifier(criterion = "gini", max_depth=10, min_samples_leaf=5)
+clf_gini.fit(X, Y)
 
-clf_entropy = DecisionTreeClassifier(criterion = "entropy", max_depth=10, min_samples_leaf=2)
-clf_entropy.fit(X_train, y_train)
+clf_entropy = DecisionTreeClassifier(criterion = "entropy", max_depth=10, min_samples_leaf=5)
+clf_entropy.fit(X, Y)
 
 # # regs = DecisionTreeRegressor(max_depth = 5)
 # regs.fit(X_train,y_train)
 
-pred_gini = clf_gini.predict(X_test)
-pred_entropy = clf_entropy.predict(X_test)
-# pred_reg = regs.predict(X_test)
+for i in range(0,100):
+	pred_gini = clf_gini.predict(X_test)
+	pred_entropy = clf_entropy.predict(X_test)
+	# pred_reg = regs.predict(X_test)
+	acc_DTE.append(accuracy_score(y_test,pred_entropy)*100)
+	acc_DTG.append(accuracy_score(y_test,pred_gini)*100)
 
-print "Tree Accuracy Using gini index: " + str(accuracy_score(y_test,pred_gini)*100)
-print "Tree Accuracy Using info gain: " + str(accuracy_score(y_test,pred_entropy)*100)
+# scores_DTE = cross_val_score(clf_entropy, X, Y, cv=300)
+# scores_DTG = cross_val_score(clf_gini, X, Y, cv=300)
+# pdb.set_trace()
+print "Tree Accuracy Using gini index: " + str(sum(acc_DTG)/len(acc_DTG))
+print "Tree Accuracy Using info gain: " + str(sum(acc_DTE)/len(acc_DTE))
 # print accuracy_score(y_test,pred_reg)
 
 doten = tree.export_graphviz(clf_entropy,out_file='tree_en.dot',feature_names=df2,filled=True,rounded=True)
